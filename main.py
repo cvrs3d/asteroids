056f93f573544c75341d5e47c05613208bded8c0
cvrs3d <ev.ustinov03@gmail.com>
2025-10-15 10:39:36 +0200

    006: Added grouping

--- main.py
+++ main.py
@@ -13,16 +13,20 @@ def main():
     running = True
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0,0,0))
-        p.draw(screen=screen)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        p.update(dt)
+        updatable.update(dt)
     pygame.quit()
 
 
