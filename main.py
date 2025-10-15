c56d47cf448baca63c8fcd5bb92fa21c1ffda69b
cvrs3d <ev.ustinov03@gmail.com>
2025-10-15 09:35:00 +0200

    001: Initialized repository

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0,0,0))
+        pygame.display.flip()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    pygame.init()
+
+    main()
